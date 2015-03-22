6f1464188679a4d2e157c116ffaaf54b5357e9ed
Derek Zhu <1and1get2@gmail.com>
2015-03-23 05:59:20 +1300

    222391364219  not working

--- Contests/ProjectEuler+/003_LargestPrimeFactor/src/Solution.java
+++ Contests/ProjectEuler+/003_LargestPrimeFactor/src/Solution.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 public class Solution {
 	public static boolean D = true;
+	private static long factor = 2;
 	static BufferedReader in = new BufferedReader(new InputStreamReader(
 			System.in));
 	static StringBuilder out = new StringBuilder();
@@ -29,13 +30,42 @@ public class Solution {
 		
 	}
 	private static void calculateCase(Long input) throws IOException{
-
-		if (D) System.out.println("Processing: " + input);
-		long largestPF = prime(input);
+//		889565456876
+		factor = 2;
 		
-		if (D) System.out.print("Final calculate: ");
-		System.out.println(largestPF);
+		long time = System.nanoTime();
+		if (D) System.out.println("Processing: " + input);
+		long largestPF;
+		
+		largestPF = prime2(input);
+		if (D) System.out.println("Final calculate: " + largestPF + " Time required to calculate in nanoseconds: " + (System.nanoTime() - time));
+		
+		largestPF = method2(input);
+		time = System.nanoTime();
+		if(D) System.out.println("Final calculate 2: " + largestPF + " Time required to calculate in nanoseconds: " + (System.nanoTime() - time));
+		
+		if (!D) System.out.println(largestPF);
 	}
+	private static long method2(long NUMBER){
+		long result = 0;
+		if (isPrime(NUMBER)) return NUMBER;
+        for(int i = 2; i < NUMBER; i++) {
+            if(NUMBER % i == 0 && isPrime(NUMBER / i)) {
+                result = NUMBER / i;
+                break;
+            }
+        }
+		return result;
+        
+	}
+	private static boolean isPrime(long l) {
+        for(long num = 2, max = l / 2 ; num < max; num++) {
+            if(l % num == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 	private static long prime(long n){
 		long i = 2;
 		while ( n % i != 0 && i < n){
@@ -45,6 +75,19 @@ public class Solution {
 		
 		if (i < n){
 			return prime(n/i);
+		} else {
+			return n;
+		}
+	}
+	private static long prime2(long n){
+		
+		while ( n % factor != 0 && factor < n){
+			factor ++;
+		}
+		if (D) System.out.println("found factor i: " + factor);
+		
+		if (factor < n){
+			return prime(n/factor);
 		} else {
 			return n;
 		}
