c9418eb6ae266ea253b04267d32392c216d1e425
Derek Zhu <1and1get2@gmail.com>
2015-03-22 15:33:07 +1300

    still timed out

--- Contests/ProjectEuler+/001_MultiplesOf3And5/src/Solution.java
+++ Contests/ProjectEuler+/001_MultiplesOf3And5/src/Solution.java
@@ -14,7 +14,7 @@ Check corner cases like n%15 == 0
 // The part of the program involving reading from STDIN and writing to STDOUT has been provided by us.
 
 public class Solution {
-	public static boolean _DEBUG_ = true;
+	public static boolean D = true;
 	static BufferedReader in = new BufferedReader(new InputStreamReader(
 			System.in));
 	static StringBuilder out = new StringBuilder();
@@ -32,11 +32,22 @@ public class Solution {
 	private static void calculateCase(Long N) throws IOException{
 		long max = N;
 		long sum = 0;
-		for (long i = 1; i < max; i++){
-			if (i%3 == 0 || i%5 == 0){
-				sum += i;
-			}
+		if (D) System.out.println("Processing: " + max);
+		long times5 = max / 5;
+		long times3 = max / 3;
+		for (int i = 1; i <= times5 && i * 5 < max; i++){
+			sum += i * 5;
+			if (D) System.out.println("adding: " + i * 5);
 		}
+		for (int i = 1; i <= times3 && i * 3 < max; i++){
+			if( i * 3 % 5 != 0){ 
+				sum += i * 3;
+				if (D) System.out.println("adding: " + i * 3);
+			}
+			
+		}
+		
+		
 		System.out.println(sum);
 	}
 }
