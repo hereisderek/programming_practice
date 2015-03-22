ae7e8e2cd2d19e0b76569ff79a590f455b6b51ec
Derek Zhu <1and1get2@gmail.com>
2015-03-22 20:41:35 +1300

    working on, save changes

---
+++ Contests/ProjectEuler+/000_ThisIsAnExample/src/Solution.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+/* Author: Derek Zhu
+ * 1and1get2@gmail.com
+ * 
+ * */
+// The part of the program involving reading from STDIN and writing to STDOUT has been provided by us.
+
+public class Solution {
+	public static boolean D = true;
+	static BufferedReader in = new BufferedReader(new InputStreamReader(
+			System.in));
+	static StringBuilder out = new StringBuilder();
+
+	public static void main(String[] args) throws NumberFormatException,
+			IOException {
+		int numOfCases = Integer.parseInt(in.readLine());
+		
+		for (int i = 0; i < numOfCases; i++){
+			calculateCase(Long.parseLong(in.readLine())); 
+		}
+		
+		
+	}
+	private static void calculateCase(Long N) throws IOException{
+
+		if (D) System.out.println("Processing: " + N);
+
+		
+		System.out.println(N);
+	}
+}
