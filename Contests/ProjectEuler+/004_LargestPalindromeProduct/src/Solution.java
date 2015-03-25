302982526af26d822d75851d1a0ad9002e4f170a
Derek Zhu <1and1get2@gmail.com>
2015-03-25 19:09:13 +1300

    #4 passed flawless, perfect

--- Contests/ProjectEuler+/004_LargestPalindromeProduct/src/Solution.java
+++ Contests/ProjectEuler+/004_LargestPalindromeProduct/src/Solution.java
@@ -31,15 +31,27 @@ public class Solution {
 	private static void calculateCase(int input) throws IOException{
 
 		if (D) System.out.println("Processing: " + input);
-		
+		int max = 0;
+		for (int i = 100; i < 1000; i++){
+			for(int j = i; j < 1000; j++){
+				if ( i*j > input) break;
+				if (isPalindrome(i * j)){
+					if (D) System.out.println("get a palindrome: " + i + "*" + j + "=" + i * j);
+					if (i * j > max) {
+						max = i * j;
+						if (D) System.out.println("got a new max: " + max);
+					}
+				}
+			}
+		}
 
-		isPalindrome(input);
-		if (D) System.out.print("Final calculate: ");
-		System.out.println(input);
+		
+		if (D) System.out.print("For input:" + input + " Final calculate: " + max);
+		if (!D) System.out.println(input);
 	}
 	
 	private static boolean isPalindrome(int number){
-		if (D) System.out.println(number + " isPalindrome?");
+		if (D) System.out.print(number + " isPalindrome?");
 		if (number <= 999999 && number >= 100000){
 			boolean b1, b2, b3;
 			b1 = (number / 100000 == (number - number / 10 * 10));
@@ -48,8 +60,15 @@ public class Solution {
 			b2 = tmp == tmp2;
 			b3 = (number - number / 10000 * 10000) / 1000 == (number - number / 1000 * 1000) / 100;
 //			if (D) System.out.println((number - number / 10000 * 10000) / 1000 + " " + (number - number / 1000 * 1000) / 100);
-			if (D) System.out.println(b1 + " " + b2 + " " + b3);
-			return (b1 && true);
-		} else return false;
+//			if (D) System.out.println(b1 + " " + b2 + " " + b3);
+//			return (b1 && true);
+			if (b1 && b2 && b3){
+				if (D) System.out.println(" true");
+				return true;
+			}
+		}
+		
+		if (D) System.out.println(" false");
+		return false;
 	}
 }
