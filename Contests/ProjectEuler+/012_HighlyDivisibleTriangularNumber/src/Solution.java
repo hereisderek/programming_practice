9a98fdc2070dca28d1d73a32ca42b9a6cabfa768
Derek Zhu <1and1get2@gmail.com>
2015-05-04 16:27:43 +1200

    no bloody idea what this question is about

--- Contests/ProjectEuler+/012_HighlyDivisibleTriangularNumber/src/Solution.java
+++ Contests/ProjectEuler+/012_HighlyDivisibleTriangularNumber/src/Solution.java
@@ -37,16 +37,42 @@ public class Solution {
 		long time = System.nanoTime();
 		long result = 0L;
 		
-		for (int i = index + 1; i <= index + input; i++){
+		for (int i = index; i <= input; i++){
+			results[i] = NumberOfDivisors(i);
+			if (D) System.out.println("For i:" + i + " Getting:" + results[i]);
+		}
+		
+		
+		
+		result = results[input];
+/*		for (int i = index + 1; i <= index + input; i++){
 			
 			results[i] = results[i-1] + i + 1;
 			if (D) System.out.println("calculating " + i + " : " + results[i]);
 			if (i == index + input) result = results[i];
-		}
+		}*/
 		
 
 		
-		if (D) System.out.println("Final calculate: " + result + " Time required to calculate in nanoseconds: " + (System.nanoTime() - time));
+		if (D) System.out.println("Final calculate: " + result + " \nTime required to calculate in nanoseconds: " + (System.nanoTime() - time));
 		if (!D) System.out.println(result);
+	}
+	private static int NumberOfDivisors(int number) {
+	    int nod = 0;
+	    int sqrt = (int) Math.sqrt(number);
+//	    long sum = 0;
+	 
+	    for(int i = 1; i<= sqrt; i++){
+	        if(number % i == 0){
+	            nod += 2;
+//	            sum += nod;
+	        }
+	    }
+	    //Correction if the number is a perfect square
+	    if (sqrt * sqrt == number) {
+	        nod--;
+	    }
+	 
+	    return nod;
 	}
 }
